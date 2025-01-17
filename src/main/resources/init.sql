0d6dc96cfa2091c408dc4e3f9e00b5da03e113f1
405516_Gonzalez_Federico <142946367+FedericoGonzalezGiordano@users.noreply.github.com>
2025-01-17 01:08:48 -0300

    new changes on dockercompose

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,7 +1,7 @@
 INSERT INTO plants (name, country, readings, med_alerts, red_alerts, sensors_disabled, status)
 VALUES
-    ('Quilmes', 'Argentina', 1250, 45, 12, 3, 1),
-    ('Cervepar', 'Paraguay', 980, 38, 8, 2, 1),
-    ('Ambev Lapa', 'Brazil', 2100, 62, 15, 5, 1),
-    ('Patricia', 'Uruguay', 850, 29, 6, 1, 1),
-    ('Brahma Rio', 'Brazil', 1850, 55, 11, 4, 1)^;
+    ('Quilmes', 'Argentina', 1250, 45, 12, 3, true),
+    ('Cervepar', 'Paraguay', 980, 38, 8, 2, true),
+    ('Ambev Lapa', 'Brazil', 2100, 62, 15, 5, true),
+    ('Patricia', 'Uruguay', 850, 29, 6, 1, true),
+    ('Brahma Rio', 'Brazil', 1850, 55, 11, 4, true);
